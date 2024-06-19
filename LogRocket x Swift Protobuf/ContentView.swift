5032448c77a7a968d34f0c22b062167c33201d4d
Brett-Best <brettbest@live.com.au>
2024-06-20 08:34:37 +1000

    Initial Commit

---
+++ LogRocket x Swift Protobuf/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  LogRocket x Swift Protobuf
+//
+//  Created by Brett Best on 20/6/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ LogRocket x Swift Protobuf/LogRocket_x_Swift_ProtobufApp.swift
@@ -0,0 +1,17 @@
+//
+//  LogRocket_x_Swift_ProtobufApp.swift
+//  LogRocket x Swift Protobuf
+//
+//  Created by Brett Best on 20/6/2024.
+//
+
+import SwiftUI
+
+@main
+struct LogRocket_x_Swift_ProtobufApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
